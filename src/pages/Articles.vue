14735f2610b2b8d06f7ea0de5f020189d64e027e
rizky.ariananda <rizky.ariananda@privyid.tech>
2025-04-12 21:16:51 +0700

    feat: add function to get list articles

--- src/pages/Articles.vue
+++ src/pages/Articles.vue
@@ -1,10 +1,101 @@
 <template>
-    <div>
-        This is Articles
+    <div class="container py-5">
+        <h3 class="fw-bold mb-4 text-center">Travel Articles</h3>
+
+        <div v-if="articleStore.error" class="text-center">
+            <BAlert variant="danger" show>{{ articleStore.error }}</BAlert>
+        </div>
+
+        <BForm @submit.prevent="onSearch" class="mb-4 row g-2 align-items-end">
+            <div class="col-md-6">
+                <BFormGroup label="Search Title">
+                    <BFormInput v-model="articleStore.searchQuery" placeholder="Search here..." />
+                </BFormGroup>
+            </div>
+            <div class="col-md-4">
+                <BFormGroup label="Category">
+                    <BFormSelect v-model="articleStore.categoryFilter" :options="categories" />
+                </BFormGroup>
+            </div>
+            <div class="col-md-2">
+                <BButton type="submit" variant="primary" class="w-100">Search</BButton>
+            </div>
+        </BForm>
+
+        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+            <div class="col" v-for="article in articleStore.articles" :key="article.id">
+                <BCard class="h-100">
+                    <BCardTitle>ID: {{ article.id }}</BCardTitle>
+                    <BCardTitle>{{ article.title }}</BCardTitle>
+                    <BCardText class="text-truncate">
+                        {{ article.description }}
+                    </BCardText>
+                    <small class="text-muted">Published: {{ formatDate(article.publishedAt) }}</small>
+                    <br/>
+                    <div class="d-flex justify-content-center">
+                        <RouterLink :to="`/articles/${article.id}`" class="btn btn-sm btn-primary mt-auto">
+                            Read More
+                        </RouterLink>
+                    </div>
+                </BCard>
+            </div>
+        </div>
+
+        <div v-if="articleStore.loading" class="text-center mt-5">
+            <BSpinner label="Loading...">
+                Loading...
+            </BSpinner>
+        </div>
     </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import { onMounted, onBeforeUnmount } from 'vue';
+import { useArticleStore } from '../store/articleStore';
+import { formatDate } from '../utils/format';
+
+const articleStore = useArticleStore()
+let scrollTimeout: ReturnType<typeof setTimeout>
+
+const categories = [
+    { value: '', text: 'All' },
+    { value: 'Nature', text: 'Nature' },
+    { value: 'Beach', text: 'Beach' },
+    { value: 'Mountain', text: 'Mountain' },
+]
+
+onMounted(() => {
+    articleStore.fetchArticles()
+    window.addEventListener('scroll', handleScroll)
+})
+
+onBeforeUnmount(()=>{
+    window.removeEventListener('scroll', handleScroll)
+})
+
+const onSearch = () => {
+  articleStore.fetchArticles(true)
+}
+
+const handleScroll = () => {
+  if (scrollTimeout) clearTimeout(scrollTimeout)
+
+  scrollTimeout = setTimeout(() => {
+    if (articleStore.loading || !articleStore.hasMore) return
+
+    const scrollableHeight = document.documentElement.scrollHeight - window.innerHeight;
+    const scrolledFromTop = window.scrollY;
+
+    if (
+        scrollableHeight - scrolledFromTop < 150 &&
+        !articleStore.loading &&
+        articleStore.hasMore
+    ) {
+      articleStore.fetchArticles()
+    
+    }
+  }, 200)
+}
 </script>
 
 <style></style>
---
+++ src/types/Article.ts
@@ -0,0 +1,39 @@
+export interface Comment {
+    id: number
+    documentId: string
+    content: string
+    createdAt: string
+    updatedAt: string
+    publishedAt: string
+    locale: string | null
+}
+  
+export interface User {
+    id: number
+    documentId: string
+    username: string
+    email: string
+    provider: string
+    confirmed: boolean
+    blocked: boolean
+    createdAt: string
+    updatedAt: string
+    publishedAt: string
+    locale: string | null
+}
+  
+export interface Article {
+    id: number
+    documentId: string
+    title: string
+    description: string
+    cover_image_url: string
+    createdAt: string
+    updatedAt: string
+    publishedAt: string
+    locale: string | null
+    user: User
+    category: string | null
+    comments: Comment[]
+    localizations?: any[]
+}
